6b48ccf686f5bd29ea196e4f27c073391107d3dc
Anna Sutheim <asutheim@gmail.com>
2023-07-24 10:55:26 -0500

    Home page components created for providers and families, boilerplate code inserted

---
+++ src/components/HomePageAdmin/HomePageAdmin.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import LogOutButton from '../LogOutButton/LogOutButton';
+import {useSelector} from 'react-redux';
+import AdminProviderTable from "../AdminProviderTable/AdminProviderTable";
+import AdminFamilyTable from "../AdminFamilyTable/AdminFamilyTable";
+
+
+function HomePageAdmin() {
+
+    const user = useSelector((store) => store.user);
+
+  return (
+    <div className="container">
+      <h1>This is the Admin Home Page</h1>
+      <h2>Welcome, {user.username}!</h2>
+        <p>Your ID is: {user.id}</p>
+        <LogOutButton className="btn" />
+      <AdminProviderTable />
+      <AdminFamilyTable />
+    </div>
+  );
+}
+
+export default HomePageAdmin;
--- src/components/HomePageFamily/HomePageFamily.jsx
+++ src/components/HomePageFamily/HomePageFamily.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
 
-function UserPage() {
+function HomePageFamily() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
     <div className="container">
+      <h1>This is a Family Home Page</h1>
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
       <LogOutButton className="btn" />
@@ -15,4 +16,4 @@ function UserPage() {
 }
 
 // this allows us to use <App /> in index.js
-export default UserPage;
+export default HomePageFamily;

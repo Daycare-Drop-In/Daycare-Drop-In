dd150a87c4e6464de7300f2744d56d969db92c3f
Anna Sutheim <asutheim@gmail.com>
2023-07-24 17:05:29 -0500

    updating input fields for new provider registration form

--- src/components/RegisterFormProvider/RegisterFormProvider.jsx
+++ src/components/RegisterFormProvider/RegisterFormProvider.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function RegisterFormProvider() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  
+
   const errors = useSelector((store) => store.errors);
   const dispatch = useDispatch();
 
@@ -11,13 +11,35 @@ function RegisterFormProvider() {
     event.preventDefault();
 
     dispatch({
-      type: 'REGISTER',
-      payload: {
-        username: username,
-        password: password,
-      },
+      type: "REGISTER",
+      payload: newProvider,
     });
-  }; // end registerUser
+  }; 
+
+const providerData = {
+  username: "",
+  password: "",
+  first_name: "",
+  last_name: "",
+  email: "",
+  phone_number: "",
+  photo_url: "",
+  license: "",
+  business_name: "",
+  street_address: "",
+  unit: "",
+  city: "",
+  state: "",
+  zip: "",
+  hours: "",
+  rates: "",
+  meals: false,
+  business_description: "",
+  personal_description: "",
+  contract_language: "",
+};
+
+const [newProvider, setNewProvider] = useState(providerData)
 
   return (
     <form className="formPanel" onSubmit={registerUser}>
@@ -33,24 +55,79 @@ function RegisterFormProvider() {
           <input
             type="text"
             name="username"
-            value={username}
+            value={newProvider.username}
             required
-            onChange={(event) => setUsername(event.target.value)}
+            onChange={(event) => setNewProvider({...newProvider, username: event.target.value})}
           />
         </label>
       </div>
+
       <div>
         <label htmlFor="password">
-          Password:
+          Password
           <input
-            type="password"
+            type="text"
             name="password"
-            value={password}
+            value={newProvider.password}
             required
-            onChange={(event) => setPassword(event.target.value)}
+            onChange={(event) => setNewProvider({...newProvider, password: event.target.value})}
           />
         </label>
       </div>
+
+      <div>
+        <label htmlFor="first_name">
+          First Name
+          <input
+            type="text"
+            name="first_name"
+            value={newProvider.first_name}
+            required
+            onChange={(event) => setNewProvider({...newProvider, first_name: event.target.value})}
+          />
+        </label>
+      </div>
+
+      <div>
+        <label htmlFor="last_name">
+          Last Name
+          <input
+            type="text"
+            name="last_name"
+            value={newProvider.last_name}
+            required
+            onChange={(event) => setNewProvider({...newProvider, last_name: event.target.value})}
+          />
+        </label>
+      </div>
+
+      <div>
+        <label htmlFor="email">
+          Email
+          <input
+            type="text"
+            name="email"
+            value={newProvider.email}
+            required
+            onChange={(event) => setNewProvider({...newProvider, email: event.target.value})}
+          />
+        </label>
+      </div>
+
+      <div>
+        <label htmlFor="phone_number">
+          Phone Number
+          <input
+            type="text"
+            name="phone_number"
+            value={newProvider.phone_number}
+            required
+            onChange={(event) => setNewProvider({...newProvider, phone_number: event.target.value})}
+          />
+        </label>
+      </div>
+
+      
       <div>
         <input className="btn" type="submit" name="submit" value="Register" />
       </div>

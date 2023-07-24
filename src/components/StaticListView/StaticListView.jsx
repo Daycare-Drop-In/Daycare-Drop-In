48f0513a65df1dd13e1dc8219378b593adf3e190
Anna Sutheim <asutheim@gmail.com>
2023-07-24 11:42:16 -0500

    Static List View and Static Provider Card components created

---
+++ src/components/StaticListView/StaticListView.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import StaticProviderCards from "../StaticProviderCards/StaticProviderCards";
+
+function StaticListView() {
+  return (
+    <div className="container">
+      <h1>Static List View</h1>
+      <p>
+        This will be the static list of all the providers who have registered
+        with the site
+      </p>
+
+      {/* This component will get mapped over in order to generate the list */}
+      <StaticProviderCards />
+    </div>
+  );
+}
+
+export default StaticListView;

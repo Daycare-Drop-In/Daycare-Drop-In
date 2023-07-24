fe377f7f196d74f318469442e40080fac3c0acaa
Anna Sutheim <asutheim@gmail.com>
2023-07-24 11:06:26 -0500

    Created component files for the provider list page, search bar, and provider cards

---
+++ src/components/ProviderListCards/ProviderListCards.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ProviderListCards () {
+    return (
+        <div className="container">
+            <h2>Provider Cards</h2>
+            <p>This will be the component that gets mapped over within 
+                the Provider List Page to display clickable info Cards
+                for each provider</p>
+        </div>
+    )
+}
+
+export default ProviderListCards
---
+++ src/components/ProviderSearchBar/ProviderSearchBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProviderSearchBar () {
+    return (
+        <div className="container">
+            <h2>Provider Search Bar</h2>
+            <p>This will be the search bar to sort the providers!</p>
+        </div>
+    )
+}
+
+export default ProviderSearchBar

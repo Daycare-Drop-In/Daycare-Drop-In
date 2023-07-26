18d7f5770e9397378d32603670de39662e96a32e
Anna Sutheim <asutheim@gmail.com>
2023-07-26 15:16:57 -0500

    Booking sagas named and outlined

--- src/redux/sagas/children.saga.js
+++ src/redux/sagas/children.saga.js
@@ -1,8 +1,25 @@
 import { put, takeLatest } from "redux-saga/effects";
 import axios from "axios";
 
+
+/*PSEUDO-CODE NOTES FOR CHILDREN SAGAS:
+
+function* getAllChildren() -- all children of a particular family 
+		yield put SET_ALL_CHILDREN
+
+function* getOneChild() -- get one child 
+		yield put SET_ONE_CHILD
+
+function* postChild() -- update info for one child
+		yield put GET_ALL_CHILDREN to call getChild function and refresh child info on dom
+
+function deleteChild() -- delete a child from the database
+
+
+*/
+
 function* childrenSaga() {
-	yield takeLatest('',);
+	yield takeLatest('GET_ALL_CHILDREN', getAllChildren);
 
 }
 

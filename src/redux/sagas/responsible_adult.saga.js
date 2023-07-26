28333b8dad6fc4c4fc1789ef64636cc6688362a7
Anna Sutheim <asutheim@gmail.com>
2023-07-26 16:09:53 -0500

    Responsible Adult sagas named and outlined

--- src/redux/sagas/responsible_adult.saga.js
+++ src/redux/sagas/responsible_adult.saga.js
@@ -1,9 +1,27 @@
 import { put, takeLatest } from "redux-saga/effects";
 import axios from "axios";
 
-function* responsibleAdultSaga() {
-	yield takeLatest('',);
+/*PSEUDO-CODE NOTES FOR RESPONSIBLE ADULT SAGAS:
 
+function* getAdults() -- all adults of a particular family 
+		yield put SET_ADULTS
+
+function* postAdult() -- add a new responsible adult to a family
+		yield put GET_ADULTS to rerender the updated list
+
+function* updateAdult() -- update info for a particular adult
+		yield put GET_ADULTS to rerender the updated list
+
+function deleteAdult() -- delete an adult 
+ 		yield put GET_ADULTS to rerender the updated list
+
+*/
+
+function* responsibleAdultSaga() {
+  yield takeLatest("GET_ADULTS", getAdults);
+  yield takeLatest("POST_ADULT", postAdult);
+  yield takeLatest("UPDATE_ADULT", updateAdult);
+  yield takeLatest("DELETE_ADULT", deleteAdult);
 }
 
 export default responsibleAdultSaga;

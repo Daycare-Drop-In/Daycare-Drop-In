037bf876a89e8200b2e710a443ed477111a98824
Ismail Ali <ishaliprime@gmail.com>
2023-07-26 15:43:17 -0500

    made admin link and protected the route and started the child form

---
+++ src/components/AddChildForm/AddChildForm.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+
+function AddChildForm() {
+    const []
+
+
+
+
+    return (
+        <>
+        </>
+    )
+}

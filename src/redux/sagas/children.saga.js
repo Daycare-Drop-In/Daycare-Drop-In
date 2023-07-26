d21819730bf171717d53a22d3205006bee51ff92
Anna Sutheim <asutheim@gmail.com>
2023-07-26 15:25:36 -0500

    Children sagas named and outlined

--- src/redux/sagas/children.saga.js
+++ src/redux/sagas/children.saga.js
@@ -4,22 +4,24 @@ import axios from "axios";
 
 /*PSEUDO-CODE NOTES FOR CHILDREN SAGAS:
 
-function* getAllChildren() -- all children of a particular family 
-		yield put SET_ALL_CHILDREN
+function* getChildren() -- all children of a particular family 
+		yield put SET_CHILDREN
 
-function* getOneChild() -- get one child 
-		yield put SET_ONE_CHILD
+function* postChild() -- add a child
+		yield put GET_CHILDREN to call getChild function and refresh child info on dom
 
-function* postChild() -- update info for one child
-		yield put GET_ALL_CHILDREN to call getChild function and refresh child info on dom
+function* updateChild() -- update info for a particular child
+		yield put SET_CHILDREN
 
 function deleteChild() -- delete a child from the database
-
 
 */
 
 function* childrenSaga() {
-	yield takeLatest('GET_ALL_CHILDREN', getAllChildren);
+	yield takeLatest('GET_CHILDREN', getChildren);
+	yield takeLatest('UPDATE_CHILD', updateChild);
+	yield takeLatest('POST_CHILD', postChild);
+	yield takeLatest('DELETE_CHILD', deleteChild)
 
 }
 

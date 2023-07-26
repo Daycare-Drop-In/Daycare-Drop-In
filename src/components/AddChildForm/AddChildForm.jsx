66b4b34758e3bdd0c684cf37821d05c9829c9f9e
Ismail Ali <ishaliprime@gmail.com>
2023-07-26 16:25:52 -0500

    halfway done with child form

--- src/components/AddChildForm/AddChildForm.jsx
+++ src/components/AddChildForm/AddChildForm.jsx
@@ -3,9 +3,20 @@ import { useDispatch, useSelector } from "react-redux";
 
 
 function AddChildForm() {
-    // const []
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [birthdate, setBirthdate] = useState("");
+    const [allergies, setAllergies] = useState("");
 
 
+
+
+
+
+
+    const registerChild = (event) => {
+
+    }
 
 
     return (

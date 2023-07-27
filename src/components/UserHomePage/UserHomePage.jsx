b191f9bda869e7b022515cf56209434767f2aedc
Anna Sutheim <asutheim@gmail.com>
2023-07-27 11:06:32 -0500

    UserHomePage component now correctly renders based on user type

--- src/components/UserHomePage/UserHomePage.jsx
+++ src/components/UserHomePage/UserHomePage.jsx
@@ -1,16 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import AdminHomePage from "../AdminHomePage/AdminHomePage";
 import ProviderHomePage from "../ProviderHomePage/ProviderHomePage";
 import FamilyHomePage from "../FamilyHomePage/FamilyHomePage";
 
 function UserHomePage() {
-  return (
-    <div className="container">
-      <AdminHomePage />
-      <FamilyHomePage />
-      <ProviderHomePage />
-    </div>
-  );
+  const user = useSelector((store) => store.user);
+  const userType = user.user_type;
+
+  console.log("Inside user home page for user: ", user);
+  console.log("user type is:", userType);
+
+  let homePage;
+
+  if (userType === "admin") {
+    homePage = <AdminHomePage />;
+  } else if (userType === "family") {
+    homePage = <FamilyHomePage />;
+  } else if (userType === "provider") {
+    homePage = <ProviderHomePage />;
+  }
+  return <div className="container">{homePage}</div>;
 }
 
 export default UserHomePage;

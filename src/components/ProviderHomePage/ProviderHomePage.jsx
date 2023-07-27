b246dedb672e804fe6ffbc72b7704c0015730bf9
Anna Sutheim <asutheim@gmail.com>
2023-07-27 16:24:27 -0500

    All provider info renders to home page component

--- src/components/ProviderHomePage/ProviderHomePage.jsx
+++ src/components/ProviderHomePage/ProviderHomePage.jsx
@@ -33,16 +33,50 @@ function ProviderHomePage() {
         <div className="provider-name"></div>
       </div>
 
-      <div className="provider-profile-photo"></div>
+      <div className="provider-profile-photo">
+        <img src={provider.provider_pic} height="200" />
+      </div>
 
-      <div className="provider-contact-info"></div>
+      <div className="provider-contact-info">
+        <p>
+          <b>Provider Name:</b> {provider.first_name} {provider.last_name}
+        </p>
+        <p>
+          <b>License:</b> {provider.license}{" "}
+        </p>
+        <p>
+          <b>Address:</b> {provider.street_address} {provider.unit}{" "}
+          {provider.city} {provider.state}
+          {provider.zip}
+        </p>
+        <p>
+          <b>Email:</b> {provider.email}
+        </p>
+      </div>
+
+      <div className="provider-bio">
+        <h2>About Me:</h2>
+        <p>{provider.personal_description}</p>
+      </div>
+
+      <div className="daycare-details">
+        <h2>About {provider.business_name}:</h2>
+        <p>
+          <b>Hours:</b> {provider.hours_open}
+          {" AM - "}
+          {provider.hours_close}
+          {"PM"}
+        </p>
+        <p>
+          <b>Meals provided? </b>
+          {provider.meals ? "Yes" : "No"}{" "}
+        </p>
+        <p>{provider.business_description}</p>
+      </div>
 
       <ProviderAvailabilityTable />
 
       <ProviderBookingProcess />
-
-      <div className="provider-bio"></div>
-      <div className="business-description"></div>
 
       <ProviderPhotoGallery />
 

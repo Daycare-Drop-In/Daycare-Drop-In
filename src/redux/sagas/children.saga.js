270852a0d354dc7eb6f00062f36c81eac93bcd9c
Anna Sutheim <asutheim@gmail.com>
2023-07-26 22:02:51 -0500

    All children sagas written

--- src/redux/sagas/children.saga.js
+++ src/redux/sagas/children.saga.js
@@ -1,29 +1,64 @@
 import { put, takeLatest } from "redux-saga/effects";
 import axios from "axios";
 
-
 /*PSEUDO-CODE NOTES FOR CHILDREN SAGAS:
 
-function* getChildren() -- all children of a particular family 
-		yield put SET_CHILDREN
 
-function* postChild() -- add a child
-		yield put GET_CHILDREN to call getChild function and refresh child info on dom
 
-function* updateChild() -- update info for a particular child
-		yield put SET_CHILDREN
-		needs an action.payload
 
-function deleteChild() -- delete a child from the database
+
+
 
 */
 
-function* childrenSaga() {
-	yield takeLatest('GET_CHILDREN', getChildren);
-	yield takeLatest('UPDATE_CHILD', updateChild);
-	yield takeLatest('POST_CHILD', postChild);
-	yield takeLatest('DELETE_CHILD', deleteChild)
+//Retrieve data for all children of a particular family ID
+function* getChildren(id) {
+  console.log("Inside getChildren saga for family of id:", id.payload);
+  try {
+    const children = yield axios.get(`/api/child/${id.payload}`);
+    yield put({ type: "SET_CHILDREN", payload: children.data });
+  } catch (error) {
+    console.log("Error in getChildren saga", error);
+  }
+}
 
+//Add a new child to a family
+function* postChild(action) {
+  console.log("Inside postChild saga:", action.payload);
+  try {
+    yield axios.post("/api/child", action.payload);
+    yield put({ type: "GET_CHILDREN" });
+  } catch {
+    console.log("Error in postChild saga", error);
+  }
+}
+// Update info for a particular child
+function* updateChild(action) {
+  console.log("Inside updateChild saga", action.payload);
+  try {
+    yield axios.put(`/api/child/${action.payload.id}`, action.payload);
+    yield put({ type: "GET_CHILDREN", payload: action.payload.id });
+  } catch (error) {
+    console.log("error in updateChild saga", error);
+  }
+}
+
+// Delete a child's info from the database
+function deleteChild(id) {
+	console.log("Inside deleteChild saga for child of ID:", id.payload);
+	try {
+	  yield axios.delete(`/api/child/${id.payload}`);
+	} catch (error) {
+	  console.log("Error in deleteChild saga", error);
+	}
+}
+
+
+function* childrenSaga() {
+  yield takeLatest("GET_CHILDREN", getChildren);
+  yield takeLatest("UPDATE_CHILD", updateChild);
+  yield takeLatest("POST_CHILD", postChild);
+  yield takeLatest("DELETE_CHILD", deleteChild);
 }
 
 export default childrenSaga;

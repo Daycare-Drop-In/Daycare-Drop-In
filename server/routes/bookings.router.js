1dd7094488263828b1492407f979c888fa142ef1
Zackaria Antar <antarzack@gmail.com>
2023-07-26 19:04:28 -0500

    added get booking information by provider id.

--- server/routes/bookings.router.js
+++ server/routes/bookings.router.js
@@ -119,7 +119,58 @@ router.get('/details/:id', (req, res) => {
 // detail view GET route template
 router.get('/provider/:id', (req, res) => {
   if (req.isAuthenticated()) {
-    pool.query()
+    const providerId = req.params.id
+    const queryText = `SELECT bookings.id AS booking_id,
+	bookings.service_date AS booked_day,
+	bookings.time_submitted AS time_booked,
+	providers.id AS provider_id,
+	providers.business_name AS biz_name,
+	providers.street_address AS provider_street,
+	providers.unit AS provider_unit,
+	providers.city AS provider_city,
+	providers.state AS provider_state,
+	providers.zip AS provider_zip,
+	providers.hours_open AS provider_open,
+	providers.hours_close AS provide_close,
+	providers.meals AS provider_meal,
+	children.id AS child_id,
+	children.family_id AS child_fam_id,
+	children.first_name AS child_first_name,
+	children.last_name AS child_last_name,
+	children.birthdate AS child_age,
+	children.allergies AS child_allergies,
+	children.potty_trained AS child_potty,
+	children.photo_url AS child_pic,
+	responsible_adults.id AS adult_id,
+	responsible_adults.first_name AS adult_first_name,
+	responsible_adults.last_name AS adult_last_name,
+	responsible_adults.phone_number AS adult_number,
+	responsible_adults.email AS adult_email,
+	responsible_adults.relationship_to_child AS adult_relationship,
+	responsible_adults.photo_url AS adult_pic,
+	responsible_adults.family_id AS adult_fam_id,
+	"user"."first_name" AS parent_first_name,
+	"user"."last_name" AS parent_last_name,
+	"user"."email" AS parent_email,
+	"user"."phone_number" AS parent_number,
+	"user"."photo_url" AS parent_pic,
+	"user"."family_id" AS parent_fam_id,
+	families.family_name AS fam_account_name,
+	families.street_address AS fam_street_address,
+	families.unit AS fam_unit,
+	families.city AS fam_city,
+	families.state AS fam_state,
+	families.zip AS fam_zip,
+	families.photo_url AS fam_pic
+FROM bookings
+	JOIN providers ON bookings.provider_id = providers.id
+	JOIN children ON bookings.child_id = children.id
+	JOIN responsible_adults ON bookings.responsible_adult_id = responsible_adults.id
+	JOIN "user" ON bookings.user_id = "user".id
+	JOIN families ON bookings.family_id = families.id
+WHERE providers.id = $1
+ORDER BY bookings.service_date ASC;`;
+    pool.query(queryText, [providerId])
     .then(() => {
       res.send(result.rows);
     })

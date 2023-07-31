2b9980aa5f6a27997bd9d8035abf8604732d00e3
Dave Nash <davidjnash2@gmail.com>
2023-07-31 01:22:01 -0500

    got hamburger menu on dom, and drawer to open with nav links inside

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 import { useSelector } from 'react-redux';
 
+
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+
+//drawer elements used
+import Drawer from "@mui/material/Drawer";
+import CloseIcon from "@mui/icons-material/Close";
+import Divider from "@mui/material/Divider";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import FolderIcon from "@mui/icons-material/Folder";
+import ImageIcon from "@mui/icons-material/Image";
+import DescriptionIcon from "@mui/icons-material/Description";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import Button from "@mui/material/Button";
+
+
+
+
 function Nav() {
   const user = useSelector((store) => store.user);
+  const [open, setState] = useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setState(open);
+  };
+
+
 
   return (
     <div className="nav">
@@ -13,41 +46,62 @@ function Nav() {
         <h2 className="nav-title">Daycare Drop-in</h2>
       </Link>
       <div>
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        )}
+        <IconButton
+          edge="start"
+          color="white"
+          aria-label="open drawer"
+          onClick={toggleDrawer(true)}
+          sx={{ color:"white", mr: 2, display: { xs: 'block', sm: 'none', }, }}>
+          <MenuIcon />
+        </IconButton>
 
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            <Link className="navLink" to="/user">
-              Home
-            </Link>
+        <Drawer
+          anchor="right" // drawer side
+          variant="temporary" // how easily the drawer closes
+          open={open} // true = drawer open
+          onClose={toggleDrawer(false)} 
+          onOpen={toggleDrawer(true)} 
+        >
 
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
-            
-            {
-              // only display if admin
-              user.user_type === "admin" && (
-                <Link className="navLink" to="/admin">
-                Admin
+          <Box>
+            {/* If no user is logged in, show these links */}
+            {!user.id && (
+              // If there's no user, show login/registration links
+              <Link className="navLink" to="/login">
+                Login / Register
+              </Link>
+            )}
+
+            {/* If a user is logged in, show these links */}
+            {user.id && (
+              <>
+                <Link className="navLink" to="/user">
+                  Home
                 </Link>
-              )
-            }
 
-            <LogOutButton className="navLink" />
-          </>
-        )}
+                <Link className="navLink" to="/info">
+                  Info Page
+                </Link>
 
-        <Link className="navLink" to="/about">
-          About
-        </Link>
+                {
+                  // only display if admin
+                  user.user_type === "admin" && (
+                    <Link className="navLink" to="/admin">
+                      Admin
+                    </Link>
+                  )
+                }
+
+                <LogOutButton className="navLink" />
+              </>
+            )}
+
+            <Link className="navLink" to="/about">
+              About
+            </Link>
+          </Box>
+        </Drawer>
+        
       </div>
     </div>
   );

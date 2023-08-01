65082afa264ef4fae3418bd02eb54443b9066d43
Anna Sutheim <asutheim@gmail.com>
2023-07-31 21:34:57 -0500

    Static public gallery components created and sourced into public provider page

---
+++ src/components/PublicProviderPhotoGallery/PublicProviderPhotoItem.jsx
@@ -0,0 +1,39 @@
+import { useHistory } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Typography from "@mui/material/Typography";
+
+function PublicProviderPhotoItem({ photo, handleDelete }) {
+  return (
+    <div className="card" id={photo.id}>
+      <Card
+        sx={{
+          width: 200,
+          height: 300,
+          padding: 1,
+          paddingBottom: 0,
+          elevation: 3,
+        }}
+        className="photo-card"
+      >
+
+        <CardMedia
+          component="img"
+          height="194"
+          image={photo.photo_url}
+          sx={{ objectFit: "contain" }}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h6" component="div">
+            {photo.description}
+          </Typography>
+
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+export default PublicProviderPhotoItem;

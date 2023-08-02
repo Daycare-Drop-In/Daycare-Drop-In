878fabf4ab6754beadfc39dd7c580df8f914e49a
Anna Sutheim <asutheim@gmail.com>
2023-08-02 17:39:35 -0500

    Provider booking table now correctly renders  all data HOORAY

--- src/components/ProviderBookingsTable/ProviderBookingsTable.jsx
+++ src/components/ProviderBookingsTable/ProviderBookingsTable.jsx
@@ -14,9 +14,30 @@ function ProviderBookingsTable() {
     dispatch({ type: "GET_PROVIDER_BOOKINGS", payload: provider_id });
   }, [provider_id]);
 
-  const booking = useSelector((store) => store.booking);
+  const bookingsArray = useSelector((store) => store.bookings);
+  console.log("HERE ARE THE BOOKINGSSSSSS:", bookingsArray);
 
-  console.log(booking)
+  const calculateAge = (birthdate) => {
+    // Convert birthdate string to a Date object
+    const birthdateObj = new Date(birthdate);
+
+    // Calculate the difference between current date and birthdate
+    const currentDate = new Date();
+    const exactAge = currentDate - birthdateObj;
+
+    // Calculate years and months
+    const ageInYears = Math.floor(exactAge / (365 * 24 * 60 * 60 * 1000));
+    const ageInMonths = Math.floor(
+      (exactAge % (365 * 24 * 60 * 60 * 1000)) / (30 * 24 * 60 * 60 * 1000)
+    );
+
+    // Format the result as a string
+    const childAge = `${ageInYears} yrs ${ageInMonths} mos`;
+
+    return childAge;
+  };
+
+ 
   return (
     <div className="container">
       <table border="1">
@@ -25,11 +46,23 @@ function ProviderBookingsTable() {
             <th>Service Date</th>
             <th>Family Name</th>
             <th>Child Name</th>
-            <th>Age Category</th>
-            <th>Drop-Off Contact</th>
+            <th>Age</th>
+            <th>Drop-Off/Pickup</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {bookingsArray?.map((booking) => (
+            <tr key={booking.booking_id}>
+              <td>{booking.booked_day}</td>
+              <td>{booking.fam_account_name}</td>
+              <td>{booking.child_first_name}</td>
+              <td>{calculateAge(booking.child_age)}</td>
+              <td>
+                {booking.adult_first_name} ({booking.adult_relationship})
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );

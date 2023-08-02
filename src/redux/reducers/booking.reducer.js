ee0188254ba21cd88ccc6cdd955a0eabc82f26de
Anna Sutheim <asutheim@gmail.com>
2023-08-02 10:12:45 -0500

    Reducers written for family and provider booking data gets

--- src/redux/reducers/booking.reducer.js
+++ src/redux/reducers/booking.reducer.js
@@ -1,11 +1,14 @@
-
 const booking = (state = [], action) => {
-    switch(action.type) {
-        case 'SET_BOOKINGS':
-            return action.payload;
-            default:
-                return state;
-    }
-}
+  switch (action.type) {
+    case "SET_BOOKINGS":
+      return action.payload;
+    case "SET_FAMILY_BOOKING_PROCESS_DATA":
+      return action.payload;
+    case "SET_PROVIDER_BOOKING_PROCESS_DATA":
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 export default booking;

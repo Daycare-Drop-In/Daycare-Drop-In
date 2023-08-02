0a17e43f7545094c47af33d39f7c1dab4c3b0092
Anna Sutheim <asutheim@gmail.com>
2023-08-02 15:29:24 -0500

    Registration for new family modified so first user is also registered as a responsible adult

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const {
-	rejectUnauthenticated,
+  rejectUnauthenticated,
 } = require("../modules/authentication-middleware");
 const encryptLib = require("../modules/encryption");
 const pool = require("../modules/pool");
@@ -10,8 +10,8 @@ const router = express.Router();
 
 // Handles Axios request for user information if user is authenticated
 router.get("/", rejectUnauthenticated, (req, res) => {
-	// Send back user object from the session (previously queried from the database)
-	res.send(req.user);
+  // Send back user object from the session (previously queried from the database)
+  res.send(req.user);
 });
 
 // ---------------------------_DEFAULT PROVIDED REGISTRATION LOGIC_---------------------------
@@ -35,31 +35,30 @@ router.get("/", rejectUnauthenticated, (req, res) => {
 
 //-------------------------------_ FAMILY USER REGISTRATION _-------------------------------------------
 router.post("/register/family", async (req, res, next) => {
-	const {
-		family_name,
-		first_name,
-		last_name,
-		address,
-		unit,
-		city,
-		state,
-		zip,
-		photo_url,
-		phone_number,
-		accessCode,
-	} = req.body;
-	const family = "family";
-	
+  const {
+    family_name,
+    first_name,
+    last_name,
+    address,
+    unit,
+    city,
+    state,
+    zip,
+    photo_url,
+    phone_number,
+    accessCode,
+  } = req.body;
+  const family = "family";
 
-	const username = req.body.username;
-	const password = encryptLib.encryptPassword(req.body.password);
+  const username = req.body.username;
+  const password = encryptLib.encryptPassword(req.body.password);
 
-	const firstQuery = `
+  const firstQuery = `
   INSERT INTO families ("family_name", "street_address", "unit", "city", "state", "zip", "photo_url", "access_code")
   VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
   RETURNING id;`;
 
-	const secondQuery = `
+  const secondQuery = `
   INSERT INTO "user" (username,
   password,
   user_type,
@@ -72,124 +71,158 @@ router.post("/register/family", async (req, res, next) => {
   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
   RETURNING id;
   `;
-	const client = await pool.connect();
-	try {
-		await client.query("BEGIN");
-		const firstStep = await client.query(firstQuery, [
-			family_name,
-			address,
-			unit,
-			city,
-			state,
-			zip,
-			photo_url,
-			accessCode,
-		]);
-		console.log("FIRST STEP COMPLETE HERES THE ID", firstStep.rows[0].id);
-		const familyId = firstStep.rows[0].id;
-		const secondStep = await client.query(secondQuery, [
-			username,
-			password,
-			family,
-			familyId,
-			first_name,
-			last_name,
-			username,
-			phone_number,
-			photo_url,
-		]);
-		await client.query("COMMIT");
-		console.log(secondStep);
-		res.sendStatus(201);
-	} catch (error) {
-		await client.query("ROLLBACK");
-		console.log("Family registration error", error);
-		res.sendStatus(500);
-	} finally {
-		console.log("THANK GOD!!!!!");
-		client.release();
-	}
+
+  const thirdQuery = `INSERT INTO "responsible_adults" (family_id, 
+	first_name, 
+	last_name, 
+	phone_number, 
+	email,
+	relationship_to_child,
+	photo_url)
+	VALUES ($1, $2, $3, $4, $5, $6, $7)`;
+
+  const client = await pool.connect();
+  try {
+    await client.query("BEGIN");
+    const firstStep = await client.query(firstQuery, [
+      family_name,
+      address,
+      unit,
+      city,
+      state,
+      zip,
+      photo_url,
+      accessCode,
+    ]);
+    console.log("FIRST STEP COMPLETE HERES THE ID", firstStep.rows[0].id);
+    const familyId = firstStep.rows[0].id;
+    const secondStep = await client.query(secondQuery, [
+      username,
+      password,
+      family,
+      familyId,
+      first_name,
+      last_name,
+      username,
+      phone_number,
+      photo_url,
+    ]);
+    console.log(secondStep);
+    const thirdStep = await client.query(thirdQuery, [
+      familyId,
+      first_name,
+      last_name,
+      phone_number,
+      username,
+      "parent",
+      photo_url,
+    ]);
+    console.log(thirdStep);
+    await client.query("COMMIT");
+    res.sendStatus(201);
+  } catch (error) {
+    await client.query("ROLLBACK");
+    console.log("Family registration error", error);
+    res.sendStatus(500);
+  } finally {
+    console.log("THANK GOD!!!!!");
+    client.release();
+  }
 });
 
 router.get(`/join/family`, (req, res) => {
-	sqlText = `SELECT families.access_code, "user".family_id FROM families 
+  sqlText = `SELECT families.access_code, "user".family_id FROM families 
 	JOIN "user" ON families.id = "user".family_id 
-	`
-	pool.query(sqlText)
-	.then((result) => {
-		res.send(result.rows)
-}).catch(err =>{
-	res.sendStatus(500)
-})
-})
+	`;
+  pool
+    .query(sqlText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+    });
+});
 
 // ----------------------------_ NEW FAMILY USER REGISTRATION _------------------------------------
 router.post("/register/new_family_user", (req, res, next) => {
-	const {
-		first_name,
-		last_name,
-		photo_url,
-		phone_number,
-		accessCode,
-		family_id, 
-	} = req.body;
+  const {
+    first_name,
+    last_name,
+    photo_url,
+    phone_number,
+    accessCode,
+    family_id,
+  } = req.body;
 
-		const family = "family";
+  const family = "family";
 
-	const username = req.body.username;
-	const password = encryptLib.encryptPassword(req.body.password);
+  const username = req.body.username;
+  const password = encryptLib.encryptPassword(req.body.password);
 
-	const firstQuery = `
+  const firstQuery = `
 	INSERT INTO "user" (username, password, user_type, family_id, first_name, last_name, email, phone_number, photo_url) 
 	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-	`
-	pool.query(firstQuery, [username, password,  family, family_id, first_name, last_name, username,  phone_number, photo_url])
-	.then((result) => {
-		res.sendStatus(201) 
-	}).catch((err)=>{
-		console.log('error in', err);
-		res.sendStatus(500);
-	})
+	`;
+  pool
+    .query(firstQuery, [
+      username,
+      password,
+      family,
+      family_id,
+      first_name,
+      last_name,
+      username,
+      phone_number,
+      photo_url,
+    ])
+    .then((result) => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log("error in", err);
+      res.sendStatus(500);
+    });
 
-	// const queryText = `INSERT INTO "user" (username, password)
-    // VALUES ($1, $2) RETURNING id`;
-	// pool.query(queryText, [username, password])
-	// 	.then(() => res.sendStatus(201))
-	// 	.catch((err) => {
-	// 		console.log("User registration failed: ", err);
-	// 		res.sendStatus(500);
-	// 	});
+  // const queryText = `INSERT INTO "user" (username, password)
+  // VALUES ($1, $2) RETURNING id`;
+  // pool.query(queryText, [username, password])
+  // 	.then(() => res.sendStatus(201))
+  // 	.catch((err) => {
+  // 		console.log("User registration failed: ", err);
+  // 		res.sendStatus(500);
+  // 	});
 });
 
 // -----------------------------_ PROVIDER REGISTRATION _-----------------------------------
 router.post("/register/provider", async (req, res, next) => {
-	const {
-		first_name,
-		last_name,
-		email,
-		phone_number,
-		photo_url,
-		license,
-		business_name,
-		street_address,
-		unit,
-		city,
-		state,
-		zip,
-		hours_open,
-		hours_close,
-		rates,
-		meals,
-		business_description,
-		personal_description,
-		contract_language,
-	} = req.body;
-	const provider = "provider";
+  const {
+    first_name,
+    last_name,
+    email,
+    phone_number,
+    photo_url,
+    license,
+    business_name,
+    street_address,
+    unit,
+    city,
+    state,
+    zip,
+    hours_open,
+    hours_close,
+    rates,
+    meals,
+    business_description,
+    personal_description,
+    contract_language,
+  } = req.body;
+  const provider = "provider";
 
-	const username = req.body.username;
-	const password = encryptLib.encryptPassword(req.body.password);
+  const username = req.body.username;
+  const password = encryptLib.encryptPassword(req.body.password);
 
-	const firstQuery = `
+  const firstQuery = `
   INSERT INTO "user" (username,
   password,
   user_type,
@@ -202,7 +235,7 @@ router.post("/register/provider", async (req, res, next) => {
   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
   RETURNING id;`;
 
-	const secondQuery = `
+  const secondQuery = `
   INSERT INTO providers (
 		"user_id",
 		"license",
@@ -224,48 +257,48 @@ router.post("/register/provider", async (req, res, next) => {
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
 RETURNING user_id;
   `;
-	const client = await pool.connect();
-	try {
-		await client.query("BEGIN");
-		const firstStep = await client.query(firstQuery, [
-			username,
-			password,
-			provider,
-			null,
-			first_name,
-			last_name,
-			username,
-			phone_number,
-			photo_url,
-		]);
-		const providerUserId = firstStep.rows[0].id;
-		const secondStep = await client.query(secondQuery, [
-			providerUserId,
-			license,
-			business_name,
-			street_address,
-			unit,
-			city,
-			state,
-			zip,
-			hours_open,
-			hours_close,
-			rates,
-			meals,
-			business_description,
-			personal_description,
-			contract_language,
-		]);
-		console.log(secondStep);
-		await client.query("COMMIT");
-		res.sendStatus(201);
-	} catch (error) {
-		await client.query("ROLLBACK");
-		console.log("Provider registration error", error);
-		res.sendStatus(500);
-	} finally {
-		client.release();
-	}
+  const client = await pool.connect();
+  try {
+    await client.query("BEGIN");
+    const firstStep = await client.query(firstQuery, [
+      username,
+      password,
+      provider,
+      null,
+      first_name,
+      last_name,
+      username,
+      phone_number,
+      photo_url,
+    ]);
+    const providerUserId = firstStep.rows[0].id;
+    const secondStep = await client.query(secondQuery, [
+      providerUserId,
+      license,
+      business_name,
+      street_address,
+      unit,
+      city,
+      state,
+      zip,
+      hours_open,
+      hours_close,
+      rates,
+      meals,
+      business_description,
+      personal_description,
+      contract_language,
+    ]);
+    console.log(secondStep);
+    await client.query("COMMIT");
+    res.sendStatus(201);
+  } catch (error) {
+    await client.query("ROLLBACK");
+    console.log("Provider registration error", error);
+    res.sendStatus(500);
+  } finally {
+    client.release();
+  }
 });
 
 // Handles login form authenticate/login POST
@@ -273,14 +306,14 @@ RETURNING user_id;
 // this middleware will run our POST if successful
 // this middleware will send a 404 if not successful
 router.post("/login", userStrategy.authenticate("local"), (req, res) => {
-	res.sendStatus(200);
+  res.sendStatus(200);
 });
 
 // clear all server session information about this user
 router.post("/logout", (req, res) => {
-	// Use passport's built-in method to log out the user
-	req.logout();
-	res.sendStatus(200);
+  // Use passport's built-in method to log out the user
+  req.logout();
+  res.sendStatus(200);
 });
 
 module.exports = router;

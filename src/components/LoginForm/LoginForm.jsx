c3dc0c58a54dbc836d34bf47d343b3e44ba09a8a
Zackaria Antar <antarzack@gmail.com>
2023-08-04 17:32:53 -0500

    added styled login form.

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import {useSelector} from 'react-redux';
+import { Button, TextField, Box, Typography, Container } from "@mui/material";
+
 
 function LoginForm() {
   const [username, setUsername] = useState('');
@@ -25,41 +27,57 @@ function LoginForm() {
   }; // end login
 
   return (
-    <form className="formPanel" onSubmit={login}>
-      <h2>Login</h2>
-      {errors.loginMessage && (
-        <h3 className="alert" role="alert">
-          {errors.loginMessage}
-        </h3>
-      )}
-      <div>
-        <label htmlFor="username">
-          Username:
-          <input
-            type="text"
-            name="username"
-            required
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="password">
-          Password:
-          <input
-            type="password"
-            name="password"
-            required
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <input className="btn" type="submit" name="submit" value="Log In" />
-      </div>
-    </form>
+		<Box
+			sx={{
+				marginTop: 8,
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+			}}
+		>
+			<Typography component="h1" variant="h5">
+				Login
+			</Typography>
+			<Box
+				component="form"
+				onSubmit={login}
+				sx={{ mt: 1 }}
+				autoComplete="off"
+			>
+				<TextField
+					margin="normal"
+					required
+					fullWidth
+					id="username"
+					value={username}
+					label="Email"
+					name="username"
+					autoFocus
+					onChange={(event) => setUsername(event.target.value)}
+					InputLabelProps={{ shrink: true }}
+				/>
+				<TextField
+					margin="normal"
+					required
+					fullWidth
+					name="password"
+					value={password}
+					label="Password"
+					type="password"
+					id="password"
+					onChange={(event) => setPassword(event.target.value)}
+					InputLabelProps={{ shrink: true }}
+				/>
+				<Button
+					type="submit"
+					fullWidth
+					variant="contained"
+					sx={{ mt: 3, mb: 2, p: 2 }}
+				>
+					Login
+				</Button>
+			</Box>
+		</Box>
   );
 }
 

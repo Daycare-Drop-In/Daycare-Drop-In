90dc600e6aacb8ae2c518b83b59007664b0767c9
Dave Nash <davidjnash2@gmail.com>
2023-08-07 00:35:24 -0500

    added preset login info click function to LoginForm

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -25,54 +25,71 @@ function LoginForm() {
     }
   }; // end login
 
+  const signIn = (event) => {
+    event.preventDefault();
+    const presetUserName = "baby shark@email.com";
+    const presetPassword = "baby shark";
+    // Populate the form inputs with the pre-set value
+    setUsername(presetUserName);
+    setPassword(presetPassword);
+  };
+
+
   return (
-    <Box
-      sx={{
-        marginTop: 8,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      {/* <Typography component="h1" variant="h5" >
+    <>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {/* <Typography component="h1" variant="h5" >
         Login
       </Typography> */}
-      <Box component="form" onSubmit={login} sx={{ mt: 1 }} autoComplete="off">
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="username"
-          value={username}
-          label="Email"
-          name="username"
-          autoFocus
-          onChange={(event) => setUsername(event.target.value)}
-          InputLabelProps={{ shrink: true }}
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          value={password}
-          label="Password"
-          type="password"
-          id="password"
-          onChange={(event) => setPassword(event.target.value)}
-          InputLabelProps={{ shrink: true }}
-        />
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="secondary"
-          sx={{ mt: 3, mb: 2, p: 2 }}
-        >
-          Login
-        </Button>
+        <Box component="form" onSubmit={login} sx={{ mt: 1 }} autoComplete="off">
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            value={username}
+            label="Email"
+            name="username"
+            autoFocus
+            onChange={(event) => setUsername(event.target.value)}
+            InputLabelProps={{ shrink: true }}
+          />
+          {/* <div
+            id="preset-username-login-info"
+            onClick={signIn}>
+          </div> */}
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            value={password}
+            label="Password"
+            type="password"
+            id="password"
+            onChange={(event) => setPassword(event.target.value)}
+            InputLabelProps={{ shrink: true }}
+            onClick={signIn}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="secondary"
+            sx={{ mt: 3, mb: 2, p: 2 }}
+          >
+            Login
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 }
 
